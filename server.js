9dd2ba7d6482ad0c56720494223f939cc413a90a
Glitch (spark-sausage) <none>
2018-06-18 10:03:53 +0000

    😹🍥 Checkpoint ./server.js:1289579/225

--- server.js
+++ server.js
@@ -21,15 +21,10 @@ app.use(cors());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/api/shorturl/new', function(req, res){
+    
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
 
 
 app.listen(port, function () {
