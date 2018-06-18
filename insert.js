87bb2fe8137dfa90f1d76d758f3a7b5fbafc8184
Glitch (spark-sausage) <none>
2018-06-18 11:08:14 +0000

    🐆🌪 Checkpoint ./insert.js:1289579/89

--- insert.js
+++ insert.js
@@ -1,6 +1,7 @@
 const { URL, Counter } = require('./schema.js');
 
 function insert(original_url){
+  console.log("INSERT FUNCTION CALLED");
   return Counter.findByIdAndUpdate({_id: "counterId"}, {$inc: {counter: 1}})
   .then(counter=>{
     console.log("New Counter: ", counter);
